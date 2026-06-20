9fe51ad96551d166ea086a0c380f449baf411060
victor <victorcamposg@gmail.com>
2026-06-19 21:04:55 -0400

    fix nota de entrega pdf

--- php/administrar/adm_venta_pdf.php
+++ php/administrar/adm_venta_pdf.php
@@ -3,7 +3,7 @@
 include("../../clases/fpdf/fpdf.php");
 include_once("adm_utilidad.php");
 
- 	/*ini_set('display_errors', 1);
+/*ini_set('display_errors', 1);
     error_reporting(E_ALL);  */
 
 
@@ -11,54 +11,61 @@ include_once("adm_utilidad.php");
 class PDF extends FPDF
 {
 
-function SetDash($black = null, $white = null) {
-    if ($black !== null) {
-        $s = sprintf('[%.3F %.3F] 0 d', $black * $this->k, $white * $this->k);
-    } else {
-        $s = '[] 0 d';
-    }
-    $this->_out($s);
-}
+	function SetDash($black = null, $white = null)
+	{
+		if ($black !== null) {
+			$s = sprintf('[%.3F %.3F] 0 d', $black * $this->k, $white * $this->k);
+		} else {
+			$s = '[] 0 d';
+		}
+		$this->_out($s);
+	}
 
-	 // Page header
-    function Header()
-    {
-        global $x_nota, $o_fecha, $o_cliente, $o_direccion, $o_cedula, $o_telefono; // Declare global variables if they are used outside the class scope and need to be accessed here
+	// Page header
+	function Header()
+	{
+		global $x_nota, $o_fecha, $o_cliente, $o_direccion, $o_cedula, $o_telefono; // Declare global variables if they are used outside the class scope and need to be accessed here
 
-        $top = 10; // Initial top position for the header elements
-        $x_header = 45; // Initial X position for the text elements
+		$top = 5; // Initial top position for the header elements
+		$x_header = 45; // Initial X position for the text elements
 		$font = 'Helvetica';
 
 		// DATOS DE LA EMPRESA
-        $this->Image('../../img/logo1.png', 19, 12, 23, 20);
+		$this->Image('../../img/logo1.png', 19, 8, 23, 20);
 
-        $this->SetXY($x_header, $top += 1);
-        $this->SetFont($font, 'B', 12);
-        $this->SetTextColor(0, 51, 102);
-        $this->Cell(50, 10, 'DISTRIBUIDORA BELLINGHIERI, F.P', 0, 0, 'L');
+		$this->SetXY($x_header, $top += 1);
+		$this->SetFont($font, '', 12);
+		$this->SetTextColor(0, 51, 102);
+		$this->Cell(50, 10, 'DISTRIBUIDORA BELLINGHIERI, F.P', 0, 0, 'L');
 
-        $this->SetTextColor(0, 51, 102);
-        $this->SetFont($font, 'B', 12);
-        $this->SetXY(150, $top);
-        $this->Cell(50, 10, 'NOTA NRO .' . $x_nota, 0, 0, 'R');
+		$this->SetTextColor(0, 51, 102);
+		$this->SetFont($font, '', 12);
+		$this->SetXY(150, $top);
+		$this->Cell(50, 10, 'NOTA NRO. ' . $x_nota, 0, 0, 'R');
 
-        $this->SetXY($x_header, $top += 5);
-        $this->SetTextColor(0, 0, 0);
-        $this->SetFont($font, 'B', 10);
-        $this->Cell(50, 10, 'RIF: V-189835180', 0, 0, 'L');
+		$this->SetXY($x_header, $top += 5);
+		$this->SetTextColor(0, 0, 0);
+		$this->SetFont($font, 'B', 10);
+		$this->Cell(50, 10, 'RIF: V-189835180', 0, 0, 'L');
 
-        $this->SetTextColor(0, 0, 0); // Black
-        $this->SetFont($font, 'B', 10);
-        $this->SetXY(150, $top);
-        $this->Cell(50, 10, fix_texto('Emisión:') . $o_fecha, 0, 0, 'R');
+		$this->SetTextColor(0, 0, 0); // Black
+		$this->SetFont($font, '', 10);
+		$this->SetXY(165, $top);
+		$this->Cell(15, 10, 'Fecha: ', 0, 0, 'R');
 
-        $this->SetXY($x_header, $top += 5);
-        $this->SetFont($font, '', 10);
-        $this->Cell(50, 10, 'CALLE 8 CASA NRO 478 URB DON IGNACIO', 0, 0, 'L');
+		$this->SetFont($font, 'B', 10);
+		$this->SetXY(180, $top);
+		$this->Cell(20, 10, $o_fecha, 0, 0, 'R');
 
-        $this->SetXY($x_header, $top += 5);
-        $this->Cell(50, 10, 'EL TIGRE ANZOATEGUI, TLF: 0424-8891559', 0, 0, 'L');
-      
+
+		$this->SetFont($font, '', 10);
+		$this->SetXY($x_header, $top += 5);
+		$this->SetFont($font, '', 10);
+		$this->Cell(50, 10, 'CALLE 8 CASA NRO 478 URB DON IGNACIO', 0, 0, 'L');
+
+		$this->SetXY($x_header, $top += 5);
+		$this->Cell(50, 10, 'EL TIGRE ANZOATEGUI, TLF: 0424-8891559', 0, 0, 'L');
+
 
 		// DATOS DEL CLIENTE
 		$top += 20;
@@ -66,7 +73,7 @@ function SetDash($black = null, $white = null) {
 		$this->SetFont($font, '', 9);
 
 		$this->SetXY(20, $top);
-		$this->Cell(100, 5, fix_texto('Razón Social:'), 'LTR', 1, 'L');		
+		$this->Cell(100, 5, fix_texto('Razón Social:'), 'LTR', 1, 'L');
 
 		$this->SetXY(120, $top);
 		$this->Cell(40, 5, fix_texto('RIF/CI:'), 'TR', 1, 'L');
@@ -76,28 +83,26 @@ function SetDash($black = null, $white = null) {
 
 		$this->SetXY(20, $top + 10);
 		$this->Cell(180, 5, 'Domicilio:', 'LR', 1, 'L');
-		
-		//CLIENT DATA 
 
-		
+		//CLIENT DATA 		
 
 		$this->SetFont($font, 'B', 10);
 		$this->SetXY(20, $top + 5);
 		$this->Cell(100, 5, fix_texto(strtoupper($o_cliente)), 'LBR', 1, 'L');
 
+		
 		$this->SetXY(120, $top + 5);
 		$this->Cell(40, 5, strtoupper($o_cedula), 'RB', 1, 'L');
 
 		$this->SetXY(160, $top + 5);
 		$this->Cell(40, 5, strtoupper($o_telefono), 'RB', 1, 'L');
 
+		$this->SetFont($font, '', 10);
 		$this->SetXY(20, $top + 15);
 		$this->MultiCell(180, 5, fix_texto($o_direccion), 'LBR', 'L');
 
 		$this->SetDash();
-        // Line break
-        //$this->Ln(20); // Add a line break after the header to give space for content
-    }
+	}
 
 	// Page footer
 	function Footer()
@@ -138,7 +143,7 @@ function fix_texto_tabla($texto)
 }
 
 $max = 110; // el max se usa para el N� de caracteres que permite pdf
-$x_movimiento =0;
+$x_movimiento = 0;
 if (!$_GET) {
 	foreach ($_POST as $nombre_campo => $valor) {
 		$asignacion = "\$" . $nombre_campo . "='" . $valor . "';";
@@ -232,16 +237,16 @@ if ($ls_resultado_1) {
 -------------------------------------------------------------------------------------------*/
 
 $font = 'Helvetica';
-$pdf = new PDF('P', 'mm', 'A4', 'UTF-8');
+$pdf = new PDF('P', 'mm', 'Letter', 'UTF-8');
 $pdf->AliasNbPages();
 $pdf->AddPage();
 $pdf->SetFont($font, 'I', 8);
 
-$top = 80;
+$top = 75;
 $ma = 20;
 $h1 = 6;
-$w1 = 20;
-$w2 = 110;
+$w1 = 15;
+$w2 = 115;
 $w3 = 25;
 $w4 = 25;
 
@@ -252,13 +257,13 @@ $pdf->SetFont($font, 'B', 10);
 $pdf->SetXY($ma, $top);
 $pdf->Cell($w1, $h1, 'Cant.', 1, 1, 'C', true);
 
-$pdf->SetXY($ma + 20, $top);
+$pdf->SetXY($ma + $w1, $top);
 $pdf->Cell($w2, $h1, 'Concepto', 1, 1, 'L', true);
 
-$pdf->SetXY($ma + 130, $top);
+$pdf->SetXY($ma + $w1 + $w2, $top);
 $pdf->Cell($w3, $h1, 'Precio', 1, 1, 'R', true);
 
-$pdf->SetXY($ma + 155, $top);
+$pdf->SetXY($ma + $w1 + $w2 + $w3, $top);
 $pdf->Cell($w4, $h1, 'Total', 1, 1, 'R', true);
 
 
@@ -270,55 +275,51 @@ while ($fila = pg_fetch_row($ls_resultado_1)) {
 	$pdf->SetFont($font, '', 10);
 	$top += 6;
 
-	$pdf->SetXY($ma, $top);
-	$pdf->Cell($w1, $h1, $fila[0], 'LR', 1, 'C');
+	$B = ($top >= 240)?'B':'';
 
-	$pdf->SetXY($ma + 20, $top);
-	$pdf->Cell($w2, $h1, strtoupper(fix_texto_tabla($arr_articulo[$fila[1]])), 'R', 1, 'L');
+	$pdf->SetXY($ma, $top);
+	$pdf->Cell($w1, $h1, $fila[0], $B.'LR', 1, 'C');
+
+	$pdf->SetXY($ma + $w1, $top);
+	$pdf->Cell($w2, $h1, strtoupper(fix_texto_tabla($arr_articulo[$fila[1]])), $B.'R', 1, 'L');
 
 	$precio = number_format($fila[2], 2, ",", ".");
-	$pdf->SetXY($ma + 130, $top);
-	$pdf->Cell($w3, $h1, $precio, 'R', 1, 'R');
+	$pdf->SetXY($ma + $w1 + $w2, $top);
+	$pdf->Cell($w3, $h1, $precio, $B.'R', 1, 'R');
 
 	$subtotal = number_format($fila[3], 2, ",", ".");
-	$pdf->SetXY($ma + 155, $top);
-	$pdf->Cell($w4, $h1, $subtotal, 'R', 1, 'R');
+	$pdf->SetXY($ma + $w1 + $w2 + $w3, $top);
+	$pdf->Cell($w4, $h1, $subtotal, $B.'R', 1, 'R');
 
 	$total += $fila[3];
 
 	if ($top >= 240) {
 		$pdf->AddPage();
-		$pdf->SetFont('Arial', 'I', 8);		
-	    $top = 80;    	
-		$ma = 20;
-		$h1 = 6;
-		$w1 = 20;
-		$w2 = 110;
-		$w3 = 25;
-		$w4 = 25;
+		$pdf->SetFont('Arial', 'I', 8);
+		$top = 80;
 
 		$pdf->SetFont($font, 'B', 10);
 		$pdf->SetXY($ma, $top);
 		$pdf->Cell($w1, $h1, 'Cant.', 1, 1, 'C', true);
-		$pdf->SetXY($ma + 20, $top);
+		$pdf->SetXY($ma + $w1, $top);
 		$pdf->Cell($w2, $h1, 'Concepto', 1, 1, 'C', true);
-		$pdf->SetXY($ma + 130, $top);
+		$pdf->SetXY($ma + $w1 + $w2, $top);
 		$pdf->Cell($w3, $h1, 'Precio', 1, 1, 'R', true);
-		$pdf->SetXY($ma + 155, $top);
+		$pdf->SetXY($ma + $w1 + $w2 + $w3, $top);
 		$pdf->Cell($w4, $h1, 'Total', 1, 1, 'R', true);
 	}
 }
 
 $top += 6;
-$pdf->SetFont($font, 'B', 11);
+$pdf->SetFont($font, 'B', 10);
 $pdf->SetXY($ma, $top);
 $pdf->Cell($w1, $h1, '', 1, 1, 'C');
-$pdf->SetXY($ma + 20, $top);
+$pdf->SetXY($ma + $w1, $top);
 $pdf->Cell($w2, $h1, '', 1, 1, 'L');
-$pdf->SetXY($ma + 130, $top);
+$pdf->SetXY($ma + $w1 + $w2, $top);
 $pdf->Cell($w3, $h1, 'Total', 1, 1, 'R');
 $temp = number_format($total, 2, ",", ".");
-$pdf->SetXY($ma + 155, $top);
+$pdf->SetXY($ma + $w1 + $w2 + $w3, $top);
 $pdf->Cell($w4, $h1, $temp, 1, 1, 'R');
 
 
